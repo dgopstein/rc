be4eec4f4d8662bd0bf11c6dadb7358eae26f7e8
Dan <dan@ongoing.ly>
2014-03-14 10:38:29 -0400

    Add install file

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Copy all git RC files from version control to ~/.xxxx files
+RC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+dot_files=`ls $RC_DIR/_*`
+
+for dot_file in $dot_files; do
+    ln -s "$dot_file" "${dot_file/\/_//.}"
+done
+
+# Configure gnome settings
+# use `dconf watch /` to observe changes on the fly
+
+dconf write /org/gnome/settings-daemon/peripherals/mouse/left-handed true # make mouse left-handed

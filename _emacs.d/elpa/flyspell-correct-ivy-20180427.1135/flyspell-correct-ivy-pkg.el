f8572467ee77bf7d24a90b257325e531d90c704f
Dan Gopstein <dan@gopstein.com>
2019-01-23 18:06:33 -0500

    packages

---
+++ _emacs.d/elpa/flyspell-correct-ivy-20180427.1135/flyspell-correct-ivy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flyspell-correct-ivy" "20180427.1135" "correcting words with flyspell via ivy interface" '((flyspell-correct "0.4.0") (ivy "0.8.0")))

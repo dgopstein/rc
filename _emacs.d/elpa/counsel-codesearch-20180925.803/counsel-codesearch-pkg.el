49177cc9c6fff896cd57b0979cdcc964f76e85dc
Dan Gopstein <dan@gopstein.com>
2024-05-26 22:42:41 -0400

    get counsel-projectile working again on osx - add files

---
+++ _emacs.d/elpa/counsel-codesearch-20180925.803/counsel-codesearch-pkg.el
@@ -0,0 +1,17 @@
+(define-package "counsel-codesearch" "20180925.803" "Counsel interface for codesearch.el"
+  '((codesearch "1")
+    (counsel "0.10.0")
+    (emacs "24")
+    (ivy "0.10.0"))
+  :commit "b7989fad3e06f301c31d5e896c42b6cc549a0e0c" :authors
+  '(("Austin Bingham" . "austin.bingham@gmail.com"))
+  :maintainers
+  '(("Austin Bingham" . "austin.bingham@gmail.com"))
+  :maintainer
+  '("Austin Bingham" . "austin.bingham@gmail.com")
+  :keywords
+  '("tools")
+  :url "https://github.com/abingham/emacs-counsel-codesearch")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ _emacs.d/elpa/yaml-pro-20240313.307/yaml-pro-pkg.el
@@ -0,0 +1,15 @@
+(define-package "yaml-pro" "20240313.307" "Parser-aided YAML editing features"
+  '((emacs "26.1")
+    (yaml "0.5.1"))
+  :commit "775c0a4b82c79da0eb91620fb0cbbf6c8642815b" :authors
+  '(("Zachary Romero"))
+  :maintainers
+  '(("Zachary Romero"))
+  :maintainer
+  '("Zachary Romero")
+  :keywords
+  '("tools")
+  :url "https://github.com/zkry/yaml-pro")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
